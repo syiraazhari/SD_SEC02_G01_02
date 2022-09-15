2b2a5c3cf5e865f3816aee7206d02ac6d851744c
Harraz Haziq <97418469+a1phanaught@users.noreply.github.com>
2022-09-15 17:36:18 +0800

    forgot password halfway done
    email sender now works properly... kisvaan please retrieve password from the database, then send password to the email

--- patient_website/public/login_signup/patient.php
+++ patient_website/public/login_signup/patient.php
@@ -1,7 +1,17 @@
 <?php
 
 session_start();
+	require __DIR__.'/PHPMailer/src/Exception.php';
+	require __DIR__.'/PHPMailer/src/PHPMailer.php';
+	require __DIR__.'/PHPMailer/src/SMTP.php';
+	//Import PHPMailer classes into the global namespace
+	//These must be at the top of your script, not inside a function
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
 
+	//Load Composer's autoloader
+	//require 'vendor/autoload.php';
 
     function addPatient() {
         $patientName = $_POST['patientName'];
@@ -57,10 +67,36 @@ session_start();
 	}
 
 	function sendPasswordToEmail() {
-		ini_set('display_errors', '1' );
 		echo "Password sent to email address";
 
-		mail("harrazhaziqazlim@gmail.com", "Password for Meinhardt Website", "Password = helloWorld123");
+		$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'harrazhaziqazlim@gmail.com';                     //SMTP username
+    $mail->Password   = 'nfcyobjnmukmcsua';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('harrazhaziqazlim@gmail.com', 'Mailer');
+    $mail->addAddress('amirah.snydex@gmail.com');    //Add a recipient              //Name is optional
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'HALLO';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 	}
 
 
