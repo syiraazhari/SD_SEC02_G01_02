7c5b6a37af8d3c469a904f5e7a4cf4a9befb987f
Harraz Haziq <97418469+a1phanaught@users.noreply.github.com>
2022-09-23 16:23:17 +0800

    forgot password completed

--- patient_website/public/login_signup/patient.php
+++ patient_website/public/login_signup/patient.php
@@ -67,36 +67,47 @@ session_start();
 	}
 
 	function sendPasswordToEmail() {
-		echo "Password sent to email address";
 
-		$mail = new PHPMailer(true);
+        $patientEmail = $_POST['forgotPasswordEmail'];
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'harrazhaziqazlim@gmail.com';                     //SMTP username
-    $mail->Password   = 'nfcyobjnmukmcsua';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        echo $patientEmail;
 
-    //Recipients
-    $mail->setFrom('harrazhaziqazlim@gmail.com', 'Mailer');
-    $mail->addAddress('amirah.snydex@gmail.com');    //Add a recipient              //Name is optional
+        $con = mysqli_connect("localhost", "web39", "web39", "meinhardt_hospital_appointment");
+        $sql = "SELECT * FROM `patient` WHERE email = '".$patientEmail."'";
+        $qry = mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($qry);
+	
+        echo "Password sent to email address";
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'HALLO';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail = new PHPMailer(true);
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'meinhardt.hospital@gmail.com';                     //SMTP username
+            $mail->Password   = 'tobecwuyasolfygr';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('meinhardt.hospital@gmail.com', 'Mailer');
+            $mail->addAddress($patientEmail);    //Add a recipient              //Name is optional
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Password Request';
+            $mail->Body    = '<p>Here is your password: '.$row['password'].'</p><p>For your safety, please change your password.</p>';
+
+            $mail->send();
+            echo 'Message has been sent';
+
+            header("Location:./passwordSent.html");
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
 	}
 
 
