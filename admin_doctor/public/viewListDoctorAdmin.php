23d1fef2b9b9cfc8a49b039ca20c7fa1b76a7c73
er <shzlord@gmail.com>
2022-10-12 03:40:04 +0800

    body

---
+++ admin_doctor/public/viewListDoctorAdmin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            table
+            {
+                
+            }
+
+            tr, td, th
+            {
+                
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Doctor List</h1>
+        <?php
+            
+        ?>
+    </body>
+</html>
