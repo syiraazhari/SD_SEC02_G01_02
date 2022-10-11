660c6fd6af2c581929b55b84ee70b300d0c4aa32
er <shzlord@gmail.com>
2022-10-12 03:47:26 +0800

    fixes

--- admin_doctor/public/viewListDoctorAdmin.php
+++ admin_doctor/public/viewListDoctorAdmin.php
@@ -2,22 +2,11 @@
 <html>
     <head>
         <style>
-            table
-            {
-                
-            }
-
-            tr, td, th
-            {
-                
-            }
+            
         </style>
     </head>
 
     <body>
-        <h1>Doctor List</h1>
-        <?php
-            
-        ?>
+        
     </body>
 </html>
