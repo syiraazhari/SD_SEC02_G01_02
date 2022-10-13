a9e06b956e1e67fd190d0b39e8cc0f85e736c552
Harraz Haziq <97418469+a1phanaught@users.noreply.github.com>
2022-10-13 16:46:21 +0800

    assign doctor completed
    admin can now assign doctors

---
+++ admin_doctor/public/doctor/doctor.php
@@ -0,0 +1,19 @@
+<?php
+
+    function assignDoctor() {
+        $name = $_POST['doctorName'];
+        $email = $_POST['doctorEmail'];
+        $password = $_POST['doctorPassword'];
+        $department = $_POST['department'];
+        $phoneNumber = $_POST['doctorPhoneNum'];
+
+        $con = mysqli_connect("localhost", "web39", "web39", "meinhardt_hospital_appointment");
+
+        $sql = "INSERT INTO `doctor` (`email`, `password`, `name`, `phoneNumber`, `department`) VALUES
+        ('".$email."', '".$password."', '".$name."', '".$phoneNumber."', '".strtoupper($department)."');";
+
+        mysqli_query($con, $sql);
+    }
+
+
+?>
---
+++ admin_doctor/public/doctor/processDoctor.php
@@ -0,0 +1,9 @@
+<?php
+
+    include "./doctor.php";
+
+    if (isset($_POST['registerDoctorBtn'])) {
+        assignDoctor();
+    }
+
+?>
